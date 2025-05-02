7cb8ba6f7900d448c9107caf777049df1bd136b2
FRANCISCONUNES09 <francisconunes76558@gmail.com>
2025-05-02 11:55:15 -0300

    Meu Projeto

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,5 +1,26 @@
+import { Link, useNavigate } from "react-router-dom"
+import svg from "/public/login-animate.svg"
+
 export default function Loginpage(){
+    const navigate = useNavigate()
+
     return (
-        <p>Aqui é a página de LOGIN</p>
+       <div className="w-full h-screen flex">
+        <div className="w-[40%] h-full flex items-center justify-center">
+           <Link to="/cadastro">Cadastrar</Link>
+           <button
+           onclick={() => navigate("/cadastro")}
+           >Cadastrar</button>
+           <button
+            onclick={() => window.location.href = "/cadastro"} 
+            >
+            Cadastrar
+            </button>
+        </div>
+        <div className="w-[60%] h-full flex bg-[#6366EE] items-center justify-center rounded-l-lg flex-col">
+            <img width={600} src={svg} alt="imagem-animda" />
+            <p className="text-white font-bold w-[300px] text-[20px] text-center">A melhor experiencia de login que você já teve na sua vida</p>
+        </div>
+       </div>
     )
 }
