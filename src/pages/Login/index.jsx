3780cd1d86e080130069a0720a2755f9e818721c
FRANCISCONUNES09 <francisconunes76558@gmail.com>
2025-05-07 14:47:45 -0300

    Meu Projeto

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -8,7 +8,7 @@ export default function Loginpage(){
        <div className="w-full h-screen flex">
         <div className="w-[40%] h-full flex items-center justify-center ">
             <div className="w-[60%] h-[40%] p-[45px] flex justify-center flex-col">
-                <h1 className="text-black font-bold text-[20px] w-full h-[60%] ">Faça seu LOGIN</h1>
+                <h1 className="text-black font-bold text-[20px] w-full h-[60%] p-4 ">Faça seu LOGIN</h1>
 
                 <label htmlFor="user" className="text-black">Usuário</label>
                 <input type="usuario" id="user"className="w-full rounded-md p-[10px] text-black m-2" placeholder="user"/>
@@ -16,7 +16,14 @@ export default function Loginpage(){
                 <label htmlFor="pass" className="text-black">senha</label>
                 <input type="password" id="pass" className="w-full rounded-md p-[10px] text-black m-2" placeholder="Password" />
 
-                <Link to="/cadastro">Cadastrar-se</Link>
+                    <p className="w-full text-[15px] text-black">Esqueci minha senha</p> 
+
+                    <button type="submit" className="w-[250px] h-[70px] rounded-md text-white
+                         bg-[#6366EE] cursor-pointer hover:bg-[#9A86F4]/80"> 
+                                  Entrar
+                </button>
+
+                    <p className="w-full h-full flex">Não tem conta ainda?<Link to="/cadastro" className="flex text-[#6366EE] font-bold pl-2">Cadastrar-se</Link></p>
             </div>
         </div>
         <div className="w-[60%] h-full flex bg-[#6366EE] items-center justify-center rounded-l-lg flex-col">
