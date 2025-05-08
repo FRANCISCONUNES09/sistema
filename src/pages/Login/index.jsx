2ceacc111adff8b480a62d093631f991c2760a34
FRANCISCONUNES09 <francisconunes76558@gmail.com>
2025-05-08 13:34:02 -0300

    Meu Projeto

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -11,20 +11,27 @@ export default function Loginpage(){
             <div className="w-[60%] h-[40%] p-[45px] flex justify-center flex-col">
                 <h1 className="text-black font-bold text-[20px] w-full h-[60%] p-4 ">Faça seu LOGIN</h1>
 
-                <label htmlFor="user" className="text-black">Usuário</label>
-                <input type="usuario" id="user"className="w-full rounded-md p-[10px] text-black m-2 bg-gray-200" placeholder="user"/>
+                <label htmlFor="user" className="text-black">E-mail</label>
+                <input type="usuario" id="user"className="w-full rounded-md p-[10px] text-black m-2 bg-gray-200" placeholder="user@gmail.com"/>
 
                 <FaRegUserCircle />
 
                 <label htmlFor="pass" className="text-black">senha</label>
                 <input type="password" id="pass" className="w-full rounded-md p-[10px] text-black m-2 bg-gray-200" placeholder="Password"  />
 
-                    <p className="w-full text-[15px] text-black p-2">Esqueci minha senha</p> 
+                <div className="w-full flex justify-between p-2">
+                <div className="w-full text-[15px] text-black"> 
+                    <input type="checkbox" />
+                    Lembra de mim
+                </div>
+                <div className=" w-full text-[15px] text-black">Esqueci minha senha</div>
+                </div>
+                     
 
                     <button type="submit" className="w-[250px] h-[70px] rounded-md text-white
                          bg-[#6366EE] cursor-pointer hover:bg-[#9A86F4]/80"> 
                                   Entrar
-                </button>
+                    </button>
 
                     <p className="w-full h-full flex">Não tem conta ainda?<Link to="/cadastro" className="flex text-[#6366EE] font-bold pl-2">Cadastrar-se</Link></p>
             </div>
